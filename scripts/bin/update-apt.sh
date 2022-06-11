30e8ee9ebdd02681724971fe97174cdd2ccd0a09
JoolsF <joolsmf@gmail.com>
2022-06-11 21:56:11 +0100

    update

---
+++ scripts/bin/update-apt.sh
@@ -0,0 +1,3 @@
+sudo apt update        # Fetches the list of available updates
+sudo apt upgrade       # Installs some updates; does not remove packages
+sudo apt autoremove    # Removes any old packages that are no longer needed
