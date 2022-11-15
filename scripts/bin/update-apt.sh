a2aa4624fa57860c33e606766331012561f319b1
Julian Fenner <joolsmf@gmail.com>
2022-11-15 09:39:26 +0000

    update

--- scripts/bin/update-apt.sh
+++ scripts/bin/update-apt.sh
@@ -1,3 +1,4 @@
 sudo apt update        # Fetches the list of available updates
 sudo apt upgrade       # Installs some updates; does not remove packages
 sudo apt autoremove    # Removes any old packages that are no longer needed
+cs update
